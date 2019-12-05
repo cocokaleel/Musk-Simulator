d0a110ecaf45115ebab364cf5053d0d20664c53e
Caroline Kaleel <cocokaleel@gmail.com>
2019-12-04 19:46:58 -0800

    Commenting on keyboardControl.js

--- keyboardControl.js
+++ keyboardControl.js
@@ -6,70 +6,78 @@ var CONTROLS = {
 
 };
 
+//sets up keyboard controls
 document.addEventListener('keydown', function(event) {
   switch (event.key) {
-    case "w":
-      ROCKET1.thrusting = true;
-      break;
-      case "a":
-      if (ROCKET1.y < GAME.canvas.height-ROCKET1.height/4 ){
-        ROCKET1.rotating = true;
-        ROCKET1.rotspeed = Math.abs(ROCKET1.rotspeed);
-      }
-        break;
-        case "d":
-        if (ROCKET1.y < GAME.canvas.height-ROCKET1.height/4 ){
-          ROCKET1.rotating = true;
-          ROCKET1.rotspeed = -Math.abs(ROCKET1.rotspeed);
-        }
-          break;
-          case "r":
-            if (!GAME.started){
-              initializeRockets();
-              EXPLOSION.currentFrame = 0;
-              GAME.started = true;
-            }
-            break;
-            case '1':
-            if (!GAME.started){
-              initializeRockets();
-              EXPLOSION.currentFrame = 0;
-              GAME.started = true;
-              GAME.mode = 1;
-            }
-            break;
-            case '2':
-            if (!GAME.started){
-              initializeRockets();
-              EXPLOSION.currentFrame = 0;
-              GAME.started = true;
-              GAME.mode = 2;
-            }
-            break;
+    //w key makes rocket thrust
+    case "w": ROCKET1.thrusting = true;
+    break;
+    
+    //a key turns on rotating variable and makes the rotation direction positive.
+    case "a":
+    if (ROCKET1.y < GAME.canvas.height-ROCKET1.height/4 ){
+      ROCKET1.rotating = true;
+      ROCKET1.rotspeed = Math.abs(ROCKET1.rotspeed);
+    }
+    break;
+    
+    //d key turns on rotating variable and makes the rotation direction negative.
+    case "d":
+    if (ROCKET1.y < GAME.canvas.height-ROCKET1.height/4 ){
+      ROCKET1.rotating = true;
+      ROCKET1.rotspeed = -Math.abs(ROCKET1.rotspeed);
+    }
+    break;
+
+    //r key "resets" game
+    case "r":
+    if (!GAME.started){
+      initializeRockets();
+      EXPLOSION.currentFrame = 0;
+      GAME.started = true;
+    }
+    break;
+
+    //1 key switches game mode?? doesn't currently do anything
+    case '1':
+    if (!GAME.started){
+      initializeRockets();
+      EXPLOSION.currentFrame = 0;
+      GAME.started = true;
+      GAME.mode = 1;
+    }
+    break;
+
+    //2 key starts multiplayer mode
+    case '2':
+    if (!GAME.started){
+      initializeRockets();
+      EXPLOSION.currentFrame = 0;
+      GAME.started = true;
+      GAME.mode = 2;
+    }
+    break;
 
     default:
-      break;
+    break;
   }
 });
 
-
+//undoes 'on' modes when buttons aren't pressed
 document.addEventListener('keyup', function(event) {
   switch (event.key) {
-    case "w":
-      ROCKET1.thrusting = false;
-      break;
-      case "a":
-        ROCKET1.rotating = false;
-        break;
-        case "d":
-          ROCKET1.rotating = false;
-          break;
-    default:
-      break;
+  case "w": ROCKET1.thrusting = false;
+  break;
+  case "a": ROCKET1.rotating = false;
+  break;
+  case "d": ROCKET1.rotating = false;
+  break;
+  default:
+  break;
   }
 });
 
-// from here its for rocket2
+// from here its for rocket2 (MULTIPLAYER)
 
 document.addEventListener('keydown', function(event) {
   switch (event.key) {
