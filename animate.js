f944204745585c411698284b6a5d6ca70789228d
Clay Skaggs <cskaggs1@hwemail.com>
2019-12-06 10:35:11 -0800

    Ur mom
    IS COOL

--- animate.js
+++ animate.js
@@ -55,6 +55,18 @@ function renderCurrentScore(context){
   context.fillText("Score: " + scorePlayerOne, 60, 70);
 
 }
+
+//when the game is paused it will display so on the screen
+function renderPaused(context){
+	if(GAME.paused){
+		context.font = "200px Arial";
+  	context.fillStyle = "red";
+  	context.fillText("PAUSED", 520, 330);
+		context.font = "75px Arial";
+		context.fillText("Click space to resume", 515, 430);
+	}
+}
+
 //draws player 2 score (Multiplayer)
 function renderCurrentScore2(context){
   context.font = "30px Arial";
@@ -101,6 +113,7 @@ function runGame() {
       renderCurrentScore(context);
       renderHighScore(context);
 
+
     }
     else{
 			handleRocketMovement();
@@ -111,6 +124,7 @@ function runGame() {
       renderFuel(context);
       renderCurrentScore(context);
       renderHighScore(context);
+			renderPaused(context);
     }
 
   }
